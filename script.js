930d310b6e2325a43d25f50c02ae74783c005f3b
Shao-NetLab <terry85324@gmail.com>
2024-03-28 14:04:16 +0800

    try to add carousel

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Carousel
+$(document).ready(function () {
+  $('.carousel[data-type="multi"] .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < 1; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+});
+
 // Enable Bootstrap tooltips
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
